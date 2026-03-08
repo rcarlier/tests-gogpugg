a9eb4add3ef23f5caba7497d2dbbbba44a687f76
rcarlier <carlier.formation@gmail.com>
2026-03-08 17:07:02 +0100

    join

---
+++ richnou_examples/join/join.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"gogpugg/internal/grid"
+	"log"
+
+	"github.com/gogpu/gg"
+)
+
+var w = 512.
+var h = 512.
+
+func main() {
+	dc := gg.NewContext(int(w), int(h))
+	dc.ClearWithColor(gg.Hex("#FFF8E1"))
+	grid.DrawGrid(dc, 0, w, 0, h, 10, "#FFE0B2", 100, "#FFB74D")
+
+	dc.SetLineWidth(10)
+
+	// Sharp corners (default)
+	dc.SetHexColor("#F5D216")
+	dc.SetLineJoin(gg.LineJoinMiter)
+	dc.DrawRectangle(50, 50, 100, 80)
+	dc.Stroke()
+
+	// Rounded corners
+	dc.SetHexColor("#0177E1")
+	dc.SetLineJoin(gg.LineJoinRound)
+	dc.DrawRectangle(200, 50, 100, 80)
+	dc.Stroke()
+
+	// Beveled (flat-cut) corners
+	dc.SetHexColor("#FD3502")
+	dc.SetLineJoin(gg.LineJoinBevel)
+	dc.DrawRectangle(350, 50, 100, 80)
+	dc.Stroke()
+
+	lineMulti(dc, 200., gg.LineJoinMiter, "#F5D216")
+	lineMulti(dc, 300., gg.LineJoinRound, "#0177E1")
+	lineMulti(dc, 400., gg.LineJoinBevel, "#FD3502")
+
+	save(dc, "join")
+}
+
+func lineMulti(dc *gg.Context, y float64, join gg.LineJoin, color string) {
+	dc.SetLineJoin(join)
+	dc.SetHexColor(color)
+	dc.MoveTo(10., y)
+	dc.LineTo(100., y)
+	dc.LineTo(130., y+50)
+	dc.LineTo(130., y+10)
+	dc.LineTo(250., y)
+	dc.LineTo(450., y)
+	dc.Stroke()
+}
+func line(dc *gg.Context, y float64, color string) {
+	dc.SetHexColor(color)
+	dc.DrawLine(10., y, 492, y)
+	dc.Stroke()
+}
+
+func save(dc *gg.Context, filename string) {
+	if err := dc.SavePNG("richnou_examples/" + filename + "/" + filename + ".png"); err != nil {
+		log.Fatalf("Failed to save PNG: %v", err)
+	}
+	log.Println("DONE")
+}
